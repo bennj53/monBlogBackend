b98d2f979fc63c3ea4336c18ad38e42ceba82b73
bennj53 <benoitjulliot@gmail.com>
2019-12-02 08:41:57 +0100

    add security config constants

--- src/main/java/com/whiterabbit/security/JWTAuthorisationFilter.java
+++ src/main/java/com/whiterabbit/security/JWTAuthorisationFilter.java
@@ -1,5 +1,13 @@
 package com.whiterabbit.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -7,22 +15,52 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class JWTAuthorisationFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        /*
-        *
-        *
-        * */
-        String jwt = httpServletRequest.getHeader("Authorization");
-        if(jwt == null) throw new RuntimeException("Not Authorized");
-        /*
-        *
-        *
-        *
-        * */
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
+        //autoriser requettes cros-domain et ajout token dans header via authorisation
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Request-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, authorization");
+        response.addHeader("Access-Control-Allow-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, authorization");
+        //si requete envoyé avec OPTION pas de recherche de token on repond OK car pas encore de token
+        if(request.getMethod().equals("OPTION")){
+            response.setStatus(HttpServletResponse.SC_OK);
+            //pas de recherche tokken non plus car accès page pour s'autentifier pas encore de token
+        }
+        //sinon recherche du token
+        else {
+
+            //recup token JWT passé dans la requette client
+            String jwt = request.getHeader(SecurityParams.JWT_HEADER_NAME);
+            if (jwt == null || !jwt.startsWith(SecurityParams.HEADER_PREFIX)) {
+                //passage au filtre suivant
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            //vérifier la signature du token (sans le prefix)
+            JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(SecurityParams.SECRET)).build();
+            DecodedJWT decodedJWT = jwtVerifier.verify(jwt.substring(SecurityParams.HEADER_PREFIX.length()));
+
+            //recup user et roles dans le token (pas besoin accès base les roles sont dans le token dans notre cas)
+            String username = decodedJWT.getSubject();
+            List<String> roles = decodedJWT.getClaims().get("roles").asList(String.class);
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            roles.forEach(roleName -> {
+                authorities.add(new SimpleGrantedAuthority(roleName));
+            });
+
+            //Spring authentifie le user
+            UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken(username, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(user);
+
+            //passer au filtre suivant
+            filterChain.doFilter(request, response);
+        }
     }
 }
--- src/main/java/com/whiterabbit/security/SecurityConfig.java
+++ src/main/java/com/whiterabbit/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.whiterabbit.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         //spring n'utilisera plus les sessions
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //autoriser accès sans aut a ces pages pour des requettes get
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/categories/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/articles/**").permitAll();
         //role necessaire pour accès aux produits et categories
         http.authorizeRequests().antMatchers("/categories/**").hasAuthority("ADMIN");
         http.authorizeRequests().antMatchers("/articles/**").hasAuthority("USER");
